c1748bede96a5bffa4d2bf7cb87277f533064694
unknown <suryasaikandikonda@gmail.com>
2019-01-27 16:14:51 +0530

    Updates

---
+++ precache-manifest.aa40e72ae7570a5b01699c3385099770.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lovecal/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ca9ae70898bfe59afbef",
+    "url": "/lovecal/static/js/runtime~main.ca9ae708.js"
+  },
+  {
+    "revision": "df211eb5c1041f574d29",
+    "url": "/lovecal/static/js/main.df211eb5.chunk.js"
+  },
+  {
+    "revision": "5fb161e1637aa45e9c43",
+    "url": "/lovecal/static/js/1.5fb161e1.chunk.js"
+  },
+  {
+    "revision": "df211eb5c1041f574d29",
+    "url": "/lovecal/static/css/main.7370e1d7.chunk.css"
+  },
+  {
+    "revision": "612b0944a8b676db477bf02aec9d3207",
+    "url": "/lovecal/index.html"
+  }
+];
